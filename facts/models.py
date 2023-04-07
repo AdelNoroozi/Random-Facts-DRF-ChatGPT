6287f89bf42aa4ca84cbf5da994a27c9e777d5f6
Adel <adel13no@gmail.com>
2023-04-07 12:10:15 +0430

    -Fact model created -Fact model registered in admin -settings modification

--- facts/models.py
+++ facts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Fact(models.Model):
+    topic = models.CharField(max_length=20, verbose_name=_('topic'))
+    fact = models.TextField(verbose_name=_('fact text'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('creation date'))
+    creator = models.CharField(max_length=20, verbose_name=_('creator'))
+
+    def __str__(self):
+        return f'{self.topic} - {self.id}'
+
+    class Meta:
+        verbose_name = _('Fact')
+        verbose_name_plural = _('Facts')
