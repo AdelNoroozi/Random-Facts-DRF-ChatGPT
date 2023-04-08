a971773d16e33a6660bbae82310ca797dca4c278
Adel <adel13no@gmail.com>
2023-04-08 11:55:46 +0430

    -openai request created

--- facts/views.py
+++ facts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import openai
 
-# Create your views here.
+from rest_framework.views import APIView
+
+
+class GenerateFact(APIView):
+    def post(self, request):
+        topic = request.data['topic']
+        openai.api_key = ""
+        openai.Model.list()
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f'tell me a random fact about {topic}',
+            temperature=0,
+            max_tokens=60,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
