8e8d2a2e0c1e864efc9203c71640ed74d942fa58
Adel <adel13no@gmail.com>
2023-04-10 22:53:20 +0430

    -authentication process added using simple jwt:  -token url added  -token refresh url added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
-from accounts.models import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 from accounts.views import *
 
 router = routers.DefaultRouter()
@@ -8,5 +11,6 @@ router.register('admins', AdminViewSet)
 urlpatterns = [
     path('add-admin/', AddAdminView.as_view(), name='add_admin'),
     path('', include(router.urls)),
-
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
